236f1171ffdb1082a2001c6ad7ce7243107d5652
Andrew Zheng <40636561+Andrew800@users.noreply.github.com>
2024-07-29 00:42:43 -0500

    Added gap between kill topology and submit topology

--- python_experiment/scripts/run_ETL_sys.sh
+++ python_experiment/scripts/run_ETL_sys.sh
@@ -46,7 +46,7 @@ cd ~/storm/riot-bench/
 
 cd -
 
-#sleep 60
+sleep 30
 
 ${home_path}storm/bin/storm jar ${home_path}storm/riot-bench/modules/storm/target/iot-bm-storm-0.1-jar-with-dependencies.jar in.dream_lab.bm.stream_iot.storm.topo.apps.ETLTopology C ETLTopologySYS ${home_source}SYS_sample_data_senml.csv SENML $inputrate   ${home_path}storm/riot-bench/output/    ${home_source}tasks.properties  test $topologyMap
 
